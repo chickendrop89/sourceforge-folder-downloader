feb689822f6ba843fe1e69a91628f4c8739018aa
chickendrop89 <chickendrop89@gmail.com>
2025-09-03 01:02:00 +0200

    license: change to using LGPL 2.1 instead
    * More fitting to this project
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- sf-downloader.sh
+++ sf-downloader.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# A recursive sourceforge folder downloader for the linux shell 
-# Copyright (C) 2025 chickendrop89
+#  A recursive sourceforge folder downloader for the linux shell
+#  Copyright (C) 2025 chickendrop89
 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
 
 SOURCE="$1"
 OUTPUT_DIRECTORY="$(pwd)"
